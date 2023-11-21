f7f39c7ee732b5fda3f2339e4c570eb961cc61d1
alijo <alijo@ALIJONOV>
2023-11-21 11:19:10 +0500

    Add project files.

---
+++ Dictionary class/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int CountNicePairs(int[] nums)
+    {
+        long  ans = 0, mod = 1000000007;
+        for (int i = 0; i < nums.Length; i++)
+            nums[i] -= rev(nums[i]);
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dic.ContainsKey(nums[i]))
+                dic[nums[i]]++;
+            else dic[nums[i] = 1;
+        }
+        foreach (var val in dic.Values)
+        {
+            ans += (val * (val - 1) / 2 % mod);
+            ans = ans % mod;
+        }
+        return (int)ans;
+    }
+    public int rev(int n)
+    {
+        int ans = 0;
+        while (n > 0)
+        {
+            ans = ans * 10 + n % 10;
+            n /= 10;
+        }
+        return ans;
+    }
+}
